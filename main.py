a33925d55e2cfe7d85eb575a2aed5824d74964d7
Andres Rodriguez <farbandresrodriguez.10@gmail.com>
2024-09-16 18:23:46 -0500

    Abordamos la sesion 2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI #Importa la clase FastAPI de la biblioteca fastapi
+from fastapi.responses import HTMLResponse #Importa la clase HTMLResponse de la biblioteca fastapi.responses
+from movies_lis import movies_list
 
 app = FastAPI() #Crea una instancia de la clase FastAPI
 app.title = 'Mi primera aplicacion de peliculas y analisis de datos'
@@ -6,5 +8,15 @@ app.version = '0.0.1'
 
 @app.get('/', tags=['home']) #Define una ruta para la API
 def message(): #Define una función de la ruta
-    return 'Hello World' #Devuelve un diccionario
+    return HTMLResponse('<h1>Hola mundo</h1>') #Retorna una respuesta HTML
 
+@app.get('/movies', tags=['Movies']) #Definiendo una ruta de la clase FastAPI
+def movies():
+    return movies_list
+
+@app.get('/movies/{id}', tags=['Movies']) #Definiendo una ruta de la clase FastAPI
+def get_movie(id:int):
+    for item in movies_list:
+        if item['id'] == id:
+            return item
+    return {}
---
+++ movies_lis.py
@@ -0,0 +1,72 @@
+movies_list = [
+    {
+        "id": 1,
+        "title": "Piratas Del Caribe 5",
+        "overview": "La venganza de salazar.",
+        "year": 1994,
+        "rating": 9.5
+    },
+    {
+        "id": 2,
+        "title": "Inception",
+        "overview": "Un ladrón que roba secretos a través de los sueños es dado la tarea de implantar una idea en la mente de un CEO.",
+        "year": 2010,
+        "rating": 8.8
+    },
+    {
+        "id": 3,
+        "title": "The Matrix",
+        "overview": "Un hacker descubre que la realidad que conoce es una simulación creada por máquinas.",
+        "year": 1999,
+        "rating": 8.7
+    },
+    {
+        "id": 4,
+        "title": "The Shawshank Redemption",
+        "overview": "Un banquero es sentenciado a cadena perpetua por un crimen que no cometió y forma una amistad con un preso veterano.",
+        "year": 1994,
+        "rating": 9.3
+    },
+    {
+        "id": 5,
+        "title": "The Godfather",
+        "overview": "La historia de una familia mafiosa en Nueva York y su lucha por el poder y la lealtad.",
+        "year": 1972,
+        "rating": 9.2
+    },
+    {
+        "id": 6,
+        "title": "Forrest Gump",
+        "overview": "La vida de un hombre con un coeficiente intelectual bajo que accidentalmente influye en eventos históricos de los años 60 y 70.",
+        "year": 1994,
+        "rating": 8.8
+    },
+    {
+        "id": 7,
+        "title": "Interstellar",
+        "overview": "Un grupo de astronautas viaja a través de un agujero de gusano en busca de un nuevo hogar para la humanidad.",
+        "year": 2014,
+        "rating": 8.6
+    },
+    {
+        "id": 8,
+        "title": "The Dark Knight",
+        "overview": "Batman enfrenta al Joker, un criminal que busca sumergir Gotham en el caos.",
+        "year": 2008,
+        "rating": 9.0
+    },
+    {
+        "id": 9,
+        "title": "Pulp Fiction",
+        "overview": "Una serie de historias entrelazadas en el mundo del crimen de Los Ángeles.",
+        "year": 1994,
+        "rating": 8.9
+    },
+    {
+        "id": 10,
+        "title": "Fight Club",
+        "overview": "Un hombre insomne crea un club de lucha clandestino que se convierte en un movimiento anárquico.",
+        "year": 1999,
+        "rating": 8.8
+    }
+]
